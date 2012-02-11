d21e2162e94498fd732ac29d8c73bdc9422723f2
Heike Hofmann <hofmann@iastate.edu>
2012-02-11 07:51:37 -0500

    first commit

--- power.R
+++ power.R
@@ -7,3 +7,4 @@ pb=c(seq(0.05,0.5, by=0.05), seq(0.25, 0.5, by=0.05))))
 powerdf$power <- with(powerdf, (1-pb)^(m-1))
 
 qplot(m, power, geom="line", group=pb, data=powerdf, colour=pb) + geom_line(aes(m, 1/m), colour="grey30", lty=2) + geom_text(aes(label=sprintf("%.2f",pb)), data=subset(powerdf, m==5), hjust=1.1) + xlim(c(0,50)) + scale_colour_gradient(name=expression(p[B]))
+ggsave(file="powerplot.pdf")
