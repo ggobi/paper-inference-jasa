1bfbf71ac2c990aeadc4c5dc965bb4f2db9261ce
Heike Hofmann <hofmann@iastate.edu>
2012-06-09 10:58:50 -0500

    lemma 3.1 final? - figure 3 fixed

--- power.R
+++ power.R
@@ -1,18 +1,26 @@
 
 library(ggplot2)
 
-powerdf <- data.frame(expand.grid(m = 5:50,
-pb=c(seq(0.05,0.5, by=0.05), seq(0.25, 0.5, by=0.05))))
+powerdf <- data.frame(expand.grid(pd = seq(0.0001, 0.20, by=0.001), m = seq(10, 30, by=5)))
+powerdf$power <- with(powerdf, pbeta(pd, 1, m-1, lower.tail=FALSE))
+powerdf$size <- with(powerdf, I(.75+(m==20)))
+powerdf$labels <- paste("m =",powerdf$m)
+powerdf$hjust <- c(-0.15,1.15)[(powerdf$m > 20) + 1]
+powerdf$power <- with(powerdf, pmax(power, 1/m))
 
-powerdf$power <- with(powerdf, (1-pb)^(m-1))
+qplot(pd, power, data=powerdf, geom="line", group=m, size=size) + ylab("Prob that data plot has lowest p-value") + xlab(expression(p[D])) + scale_size_identity() +geom_text(aes(x=pd, y=power, label=labels, hjust=hjust), size=3, data=subset(powerdf, abs(4*pd - power) < 0.005 )) + geom_hline(y=1/seq(15,30, by=5), linetype=2, alpha=0.3) 
 
-K <- 10
-ggplot(aes(x=m, power,  group=pb, colour=pb), data=powerdf) + geom_line(aes(m, 1/m), colour="grey30", lty=2) + geom_text(aes(label=sprintf("%.2f", pb), show_guide=FALSE), data=subset(powerdf, m==5), hjust=1.1) + xlim(c(0,50)) + scale_colour_gradient(name=expression(p[B])) + geom_ribbon(aes(x=m, ymin=1/m-1.96*sqrt(1/m*(m-1)/m)/K, ymax=1/m+1.96*sqrt(1/m*(m-1)/m)/K), fill="grey80", lty=0, alpha=0.1) +
-geom_line()
 ggsave(file="powerplot.pdf")
 
 
+
+
+
 powerdf$EX <- with(powerdf, (m-1)*(1-pb))
+
+
+ggplot(aes(x=pb, m,  group = power, colour=power), data=powerdf) + geom_point()
+
 
 
 qplot(m, EX, geom="line", group=pb, data=powerdf, colour=pb) + geom_line(aes(m, 1/m), colour="grey30", lty=2)  + geom_text(aes(label=sprintf("%.2f",pb)), data=subset(powerdf, m==5), hjust=1.1) + xlim(c(0,50)) + scale_colour_gradient(name=expression(p[B]))
